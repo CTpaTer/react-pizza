107effd6e2741dce096c16535b65d56649e6d07e
Pavel Demaev <ctpater@gmail.com>
2023-04-06 16:43:11 +0300

    init: project react-pizza

---
+++ src/components/Categories.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export function Categories() {
+  const [activeIndex, setActiveIndex] = React.useState(0);
+
+  const categories = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые'];
+
+  return (
+    <div className="categories">
+      <ul>
+        {categories.map((value, index) => {
+          return (
+            <li
+              key={value}
+              onClick={() => setActiveIndex(index)}
+              className={activeIndex === index ? 'active' : ''}>
+              {value}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
