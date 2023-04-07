d2f23385b8e5c0b5c5618100985b6ce882ddaf81
Pavel Demaev <ctpater@gmail.com>
2023-04-07 13:41:35 +0300

    feat: add add skeleton for preloading pizza card

--- src/App.js
+++ src/App.js
@@ -7,16 +7,19 @@ import { Header } from './components/Header';
 import { Categories } from './components/Categories';
 import { Sort } from './components/Sort';
 import { PizzaBlock } from './components/PizzaBlock';
+import { Skeleton } from './components/PizzaBlock/Sceleton';
 
 function App() {
   const apiUrl = 'https://642ed4682b883abc64182eca.mockapi.io/items';
 
   const [items, setItems] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
 
   React.useEffect(() => {
     try {
       axios.get(`${apiUrl}`).then((arr) => {
         setItems(arr.data);
+        setIsLoading(false);
       });
     } catch (error) {
       console.log(error);
@@ -34,9 +37,9 @@ function App() {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            {items.map((obj) => (
-              <PizzaBlock key={obj.id} {...obj} />
-            ))}
+            {isLoading
+              ? [...new Array(8)].map((_, index) => <Skeleton key={index} />)
+              : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
           </div>
         </div>
       </div>
